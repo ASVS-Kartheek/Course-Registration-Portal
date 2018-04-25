1c522e5504b6f11becb8373d129db4792a293e3d
Kartheek Akella <sukruthkartheek@gmail.com>
2018-04-25 09:53:22 +0530

    modify column names in users
    change type column in users table to type_of_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_134858) do
+ActiveRecord::Schema.define(version: 2018_04_25_041902) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.string "type"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type_of_users"
   end
 
 end
